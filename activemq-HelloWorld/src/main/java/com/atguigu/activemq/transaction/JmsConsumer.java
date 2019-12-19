7cf545cbd86cb41eb4250eb85f2896514d650c17
wojiaochadi <264835229@qq.com>
2019-12-19 14:44:46 +0800

    activemq的事物
    事物 偏 生产者
    签收 偏 消费者

---
+++ activemq-HelloWorld/src/main/java/com/atguigu/activemq/transaction/JmsConsumer.java
@@ -0,0 +1,58 @@
+package com.atguigu.activemq.transaction;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+public class JmsConsumer {
+
+    public static String ACTIVEMQ_USER = "admin";
+    public static String ACTIVEMQ_PASSWORD = "admin";
+    public static String ACTIVEMQ_BROKER_URL = "tcp://192.168.198.129:61616";
+    public static String QUEUE_NAME = "queue_transaction";
+
+    public static void main(String[] args) throws JMSException {
+        //1 创建连接工场,使用默认用户名密码
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_USER, ACTIVEMQ_PASSWORD, ACTIVEMQ_BROKER_URL);
+
+        //2 获得连接并启动
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.start();
+
+        //3 创建会话,此步骤有两个参数，第一个是否以事务的方式提交，第二个默认的签收方式
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        //4.创建目的地，具体可以是队列 也可以是 主题
+        //Destination destination = session.createQueue(QUEUE_NAME);
+        Queue queue = session.createQueue(QUEUE_NAME);
+
+        //======消费者不同的地方======
+        //5.创建消费者
+        MessageConsumer messageConsumer = session.createConsumer(queue);
+
+        /*
+         * 同步堵塞方式( receive() )
+         * 订阅者或接收者调用 MessageConsumer.receive()方法接收消息，receive方法在能够接收到消息之前（或超时之前）将一直阻塞。
+         */
+        while(true){
+            //6.消费者获取一条消息（此时mq控制台里面会少一条消息）  receive()方法参数为空，则会一直等待
+            //TextMessage textMessage = (TextMessage) messageConsumer.receive();
+            TextMessage textMessage = (TextMessage) messageConsumer.receive(4000L); //添加recive()添加参数，则4秒后自动关闭进程
+            if(null != textMessage){
+                System.out.println("***消费者收到消息***：" + textMessage.getText());
+            }else{
+                break;
+            }
+        }
+
+        messageConsumer.close();
+        session.close();
+        connection.close();
+        System.out.println("***queue_transaction_消费者接受消息完成！！！***");
+    }
+}
---
+++ activemq-HelloWorld/src/main/java/com/atguigu/activemq/transaction/JmsProduce.java
@@ -0,0 +1,69 @@
+package com.atguigu.activemq.transaction;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.Connection;
+import javax.jms.DeliveryMode;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+public class JmsProduce {
+
+    public static String ACTIVEMQ_USER = "admin";
+    public static String ACTIVEMQ_PASSWORD = "admin";
+    public static String ACTIVEMQ_BROKER_URL = "tcp://192.168.198.129:61616";
+    public static String QUEUE_NAME = "queue_transaction";
+
+    public static void main(String[] args) throws JMSException {
+
+        //1 创建连接工场,使用默认用户名密码
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_USER, ACTIVEMQ_PASSWORD, ACTIVEMQ_BROKER_URL);
+
+        //2 获得连接并启动
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.start();
+
+        //3 创建会话,此步骤有两个参数，第一个是否以事务的方式提交，第二个默认的签收方式
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        //4.创建目的地，具体可以是队列 也可以是 主题
+        //Destination destination = session.createQueue(QUEUE_NAME);
+        Queue queue = session.createQueue(QUEUE_NAME);
+
+        //======生产者不同的地方======
+        //5.创建消息的生产者（此时mq控制台，会创建queue01的队列）
+        MessageProducer messageProducer = session.createProducer(queue);
+
+        //6.通过使用messageProducer 产生3条消息到队列里面
+        for (int i = 0; i < 6; i++) {
+            //7.创建消息
+            TextMessage textMessage = session.createTextMessage("This is msg: " + i);//字符串消息
+
+            //设置消息的额外参数，也可以在 messageProducer.send方法中进行设置
+            //设置消息的目的地，可以重新设置目的地
+            textMessage.setJMSDestination(queue);
+            //设置消息的持久性
+            textMessage.setJMSDeliveryMode(DeliveryMode.PERSISTENT);
+            //消息的过期时间（默认永不过期）
+            textMessage.setJMSExpiration(1000L);
+            //消息的优先级，0-9 十个级别， 0-4 普通消息  5-9 加急消息
+            textMessage.setJMSPriority(9);
+            //消息的id，唯一识别编号
+            textMessage.setJMSMessageID("MsgId_" + i);
+
+            //8.通过messageProducer发给mq（此时mq控制台的队列里面会多一条消息）  设置消息的额外参数，有多种重写的方法
+            messageProducer.send(textMessage);
+
+        }
+
+        //9.关闭消息
+        messageProducer.close();
+        session.close();
+        connection.close();
+
+        System.out.println("MQ_queue_transaction消息发送完成！");
+    }
+}
